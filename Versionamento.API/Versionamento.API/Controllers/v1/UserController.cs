604f1473b918becdc7931ed31f5f519de8467a04
Filipe Brito <filipe.brito@hubconexa.com>
2023-12-24 11:23:44 -0300

    primeiro commit

---
+++ Versionamento.API/Versionamento.API/Controllers/v1/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Versionamento.API.Controllers.v1
+{
+    [ApiVersion(1.0)]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("User V1");
+        }
+    }
+}
---
+++ Versionamento.API/Versionamento.API/Program.cs
@@ -0,0 +1,47 @@
+using Asp.Versioning.ApiExplorer;
+using Versionamento.API.SwaggerConfig;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.OperationFilter<SwaggerDefaultValues>();
+});
+builder.Services.AddApiVersioning()
+    .AddMvc()
+    .AddApiExplorer(setup =>
+    {
+        setup.GroupNameFormat = "'v'VVV";
+        setup.SubstituteApiVersionInUrl = true; 
+    });
+
+builder.Services.ConfigureOptions<ConfigureSwaggerGenOptions>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        var version = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+        foreach (var description in version.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", $"Web Api - {description.GroupName.ToUpper()}");
+        }
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
